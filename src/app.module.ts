2b4edab65ff6a071ae057653a44bb3ff28bb55c3
Giovanni Mosquera <Giovanni2414g@gmail.com>
2023-10-15 23:47:35 -0500

    Added AUTH with incomplete database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'mypassword',
+      database: 'ecommerce',
+      entities: [],
+      synchronize: true,})
+    ],
 })
 export class AppModule {}
